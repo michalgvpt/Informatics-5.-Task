f09f653e1769ce0a848bdcd5ea07d8557e8c4f30
G3nHawkeye <113044132+michalgvpt@users.noreply.github.com>
2022-10-18 20:25:52 +0200

    Add files via upload

--- def/1.uloha.py
+++ def/1.uloha.py
@@ -1,7 +1,3 @@
-#width=int(input('Give me width of rectagle: '))
-#symbol=str(input('Give me a symbol: '))
-#rectangle(width,symbol)
-
 def rectangle(width,symbol):
     print(symbol*width)
     print(symbol+' '*(width-2)+symbol)
