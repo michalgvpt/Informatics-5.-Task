c9f1da732e7cb3d04b1aedb49f9caa9c8b3c779b
G3nHawkeye <113044132+michalgvpt@users.noreply.github.com>
2022-10-18 19:49:23 +0200

    Delete for directory

--- for/4.uloha.py
+++
@@ -1,5 +0,0 @@
-n=int(input("n: "))
-sum=0
-for i in range(1,n+1):
-    sum=sum+i
-print(sum)
--- for/6.uloha.py
+++
@@ -1,7 +0,0 @@
-x=int(input("x: "))
-y=int(input("y: "))
-count=0
-for i in range(x,y+1):
-    if i%8 == 0:
-        count += 1
-print(count)
