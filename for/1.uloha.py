595ff217c2e9b1597419be35fde3e95259c78ebb
G3nHawkeye <113044132+michalgvpt@users.noreply.github.com>
2022-10-24 20:28:17 +0200

    Delete for directory

--- for/1.uloha.py
+++
@@ -1,5 +0,0 @@
-n=int(input("n: "))
-
-for i in range(1,n+1):
-    if (i%3 == 0):
-        print(i)
--- for/5.uloha.py
+++
@@ -1,6 +0,0 @@
-n=int(input("n: "))
-count=0
-for i in range(1,n+1):
-    if (i%2 == 0):
-        count += 1
-print(count)
